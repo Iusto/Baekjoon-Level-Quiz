0ba44fb79413e887aab92498b7ee67349534ae72
Iusto <85227042+Iusto@users.noreply.github.com>
2022-01-07 10:24:42 +0900

    Baekjoon:10871
    A만큼의 정수들 중에서 X보다 작은 수만 출력

---
+++ src/Xsmall.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Xsmall {
+ 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+ 
+		int a = Integer.parseInt(st.nextToken());
+		int X = Integer.parseInt(st.nextToken());
+ 
+		StringBuilder sb = new StringBuilder();
+ 
+		st = new StringTokenizer(br.readLine(), " ");
+		
+		for (int i = 1; i <= a; i++) {
+			int value = Integer.parseInt(st.nextToken());
+			if (value < X)
+				sb.append(value).append(" ");
+		}
+		System.out.print(sb);
+		br.close();
+	}
+}
